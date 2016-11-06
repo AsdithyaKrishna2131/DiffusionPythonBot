120889da7b11e6c8366ec782698bf2adb42827df
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2016-11-06 16:07:00 +0800

    chore: config

--- diffusion_python_bot/user_commands.py
+++ diffusion_python_bot/user_commands.py
@@ -20,3 +20,5 @@ class UserCommands(commands.Cog):
             steps = config.get_user_setting(user_id, "steps", 50)
             negative_prompt = config.get_user_setting(
                 user_id,
+                "negative_prompt",
+                "(child, baby, deformed, distorted, disfigured:1.3), poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, (mutated hands and fingers:1.4), disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation",
