29296312ba71be9490d03152e4449e3bd02558ad
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2016-11-09 11:06:00 +0800

    fix: pipeline

--- diffusion_python_bot/user_commands.py
+++ diffusion_python_bot/user_commands.py
@@ -35,3 +35,5 @@ class UserCommands(commands.Cog):
             f"🟠 **Model ID**: `{model_id}`\n❓ Change using **!setmodel [model]**, out of the list from **!listmodels**\n"
             f"🟠 **Steps**: `{steps}`\n❓ This represents how many denoising iterations the model will do on your image. Less is more.\n"
             f"🟠 **Negative Prompt:**:\n➡️    `{negative_prompt}`\n❓ Images featuring these keywords are less likely to be generated. Set via `!negative`.\n"
+            f"🟠 **Positive Prompt:**:\n➡️    `{positive_prompt}`\n❓ Added to the end of every prompt, which has a limit of 77 tokens. This can become truncated. Set via `!positive`.\n"
+            f"🟠 **Resolution:** `{resolution['width']}x{resolution['height']}`\n❓ Lower resolutions render more quickly, and has a relationship with `steps` that can really influence the output. See **!help resolution** for more information."
