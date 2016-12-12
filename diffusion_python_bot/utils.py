30c489ea2e1bef5369c36424f1a85d4debc435cf
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2016-12-12 13:27:00 +0800

    style: clean up config

--- diffusion_python_bot/utils.py
+++ diffusion_python_bot/utils.py
@@ -5,3 +5,4 @@ import tomlkit
 
 def _get_project_meta():
     pyproject_path = os.path.join(
+        (os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(sys.argv[0])))))),
