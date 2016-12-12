0444002369e1ca8f20735280a1ff9c1091393cef
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2016-12-12 13:45:00 +0800

    refactor: tidy docs

--- diffusion_python_bot/utils.py
+++ diffusion_python_bot/utils.py
@@ -6,3 +6,4 @@ import tomlkit
 def _get_project_meta():
     pyproject_path = os.path.join(
         (os.path.abspath(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(sys.argv[0])))))),
+        "pyproject.toml",
