389ad2a7f3c0258a445e7aa0184d095bbd1e0b85
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2016-12-23 19:07:00 +0800

    test: cover progress

--- diffusion_python_bot/classes/app_config.py
+++ diffusion_python_bot/classes/app_config.py
@@ -5,3 +5,9 @@ import os
 import sys
 from io import BytesIO
 
+
+class AppConfig:
+    def __init__(self):
+        from pathlib import Path
+
+        parent = os.path.dirname(Path(__file__).resolve().parent)
