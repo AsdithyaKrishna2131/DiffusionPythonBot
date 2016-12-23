be02403d07c9894c825c0fb35a9758b6ce18d17f
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2016-12-23 19:25:00 +0800

    test: cover progress

--- diffusion_python_bot/classes/app_config.py
+++ diffusion_python_bot/classes/app_config.py
@@ -11,3 +11,9 @@ class AppConfig:
         from pathlib import Path
 
         parent = os.path.dirname(Path(__file__).resolve().parent)
+        config_path = os.path.join(parent, "config")
+        self.config_path = os.path.join(config_path, "config.json")
+        self.example_config_path = os.path.join(config_path, "example.json")
+
+        if not os.path.exists(self.config_path):
+            with open(self.example_config_path, "r") as example_file:
