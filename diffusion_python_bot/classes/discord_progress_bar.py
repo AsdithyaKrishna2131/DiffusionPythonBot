df103a1ffe539a6d924ae9d24ecf34a3f9a214db
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2017-03-20 13:43:00 +0800

    chore: bump config

--- diffusion_python_bot/classes/discord_progress_bar.py
+++ diffusion_python_bot/classes/discord_progress_bar.py
@@ -4,3 +4,5 @@ class DiscordProgressBar:
         self.total_steps = total_steps
         self.progress_bar_length = progress_bar_length
         self.original_stdout = original_stdout
+        self.current_step = 0
+        self.progress_message = progress_message
