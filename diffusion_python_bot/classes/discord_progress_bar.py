da93cab8e3e9099be52eec22b0bad205ad95e4dd
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2017-03-20 13:00:00 +0800

    refactor: tidy images

--- diffusion_python_bot/classes/discord_progress_bar.py
+++ diffusion_python_bot/classes/discord_progress_bar.py
@@ -1,2 +1,4 @@
 class DiscordProgressBar:
     def __init__(self, ctx, total_steps, original_stdout, progress_bar_length=20, progress_message=None):
+        self.ctx = ctx
+        self.total_steps = total_steps
