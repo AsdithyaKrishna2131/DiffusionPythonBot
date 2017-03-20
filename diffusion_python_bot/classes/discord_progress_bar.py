aec2cf4ba750ecb7e1ad70a1d8089bc92c8e4789
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2017-03-20 13:18:00 +0800

    feat: discord

--- diffusion_python_bot/classes/discord_progress_bar.py
+++ diffusion_python_bot/classes/discord_progress_bar.py
@@ -2,3 +2,5 @@ class DiscordProgressBar:
     def __init__(self, ctx, total_steps, original_stdout, progress_bar_length=20, progress_message=None):
         self.ctx = ctx
         self.total_steps = total_steps
+        self.progress_bar_length = progress_bar_length
+        self.original_stdout = original_stdout
