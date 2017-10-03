c19efb33f3d5290f6b508953e9b0c8df10ffed64
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2017-10-03 18:09:00 +0800

    chore: bump bot

--- diffusion_python_bot/classes/model_downloader.py
+++ diffusion_python_bot/classes/model_downloader.py
@@ -7,3 +7,6 @@ class ModelDownloader:
         self.local_models = []
         self.base_dir = "."
         self.download_lock = threading.Lock()
+
+    def download_model(self, subdir):
+        with self.download_lock:
