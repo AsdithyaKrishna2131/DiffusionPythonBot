dea4a819d35b7180e145152905bd9fd5a6f8cac2
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2017-10-17 18:53:00 +0800

    test: cover docs

--- diffusion_python_bot/classes/model_list.py
+++ diffusion_python_bot/classes/model_list.py
@@ -5,3 +5,9 @@ import sys
 from io import BytesIO
 
 
+class ModelList:
+    def __init__(self):
+        from pathlib import Path
+        parent = os.path.dirname(Path(__file__).resolve().parent)
+        model_list_path = os.path.join(parent, "config")
+        self.model_list_path = os.path.join(model_list_path, "config.json")
