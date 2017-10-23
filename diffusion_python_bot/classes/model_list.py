47f3a6a6feecaca5477f99c81df784150be6c7a0
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2017-10-23 09:32:00 +0800

    style: clean up progress

--- diffusion_python_bot/classes/model_list.py
+++ diffusion_python_bot/classes/model_list.py
@@ -28,3 +28,8 @@ class ModelList:
     def get_command_prefix(self):
         return self.config.get("cmd_prefix", "+")
 
+    def get_max_resolution_by_aspect_ratio(self, aspect_ratio: str):
+        return self.config.get("maxres", {}).get(aspect_ratio, {"width": self.get_max_resolution_width(aspect_ratio=aspect_ratio), "height": self.get_max_resolution_height(aspect_ratio=aspect_ratio)})
+
+    def get_max_resolution_width(self, aspect_ratio: str):
+        return self.config.get("maxres", {}).get(aspect_ratio, {}).get("width", 800)
