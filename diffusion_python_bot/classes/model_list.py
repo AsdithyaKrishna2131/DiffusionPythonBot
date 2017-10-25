3bb3a2960e37435bc154760e5656b2a6e09a8fd9
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2017-10-25 10:25:00 +0800

    fix: bot

--- diffusion_python_bot/classes/model_list.py
+++ diffusion_python_bot/classes/model_list.py
@@ -45,3 +45,8 @@ class ModelList:
 
     def get_huggingface_api_key(self):
         return self.config["huggingface_api"].get("api_key", None)
+
+    def get_local_model_path(self):
+        return self.config["huggingface"].get("local_model_path", "/root/.cache/huggingface")
+
+    def get_imgur_config(self):
