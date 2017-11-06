745248a4f2918e5c3e00b08ee0b6825c6daadfd9
EmiColeman780 <309199564+EmiColeman780@users.noreply.github.com>
2017-11-06 10:15:00 +0800

    feat: progress

--- diffusion_python_bot/classes/pytorch_stderr_filter.py
+++ diffusion_python_bot/classes/pytorch_stderr_filter.py
@@ -1,3 +1,4 @@
 class FilteringStderrWrapper:
     def __init__(self, original_stderr, original_stdout):
         self.original_stderr = original_stderr
+        self.original_stdout = original_stdout
