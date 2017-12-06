642b1535dd0874816d9362c26d44284a5c378152
aLima89 <309020643+aLima89@users.noreply.github.com>
2017-12-06 09:59:00 +0800

    test: cover images

--- diffusion_python_bot/classes/tqdm_capture.py
+++ diffusion_python_bot/classes/tqdm_capture.py
@@ -1,3 +1,5 @@
 import re, asyncio
 from diffusion_python_bot.classes.discord_progress_bar import DiscordProgressBar
 
+class TqdmCapture:
+    def __init__(self, progress_bar: DiscordProgressBar, loop, original_stdout, original_stderr):
