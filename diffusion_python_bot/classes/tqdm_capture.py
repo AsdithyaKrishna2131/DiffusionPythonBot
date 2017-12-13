e2761c9a1a5784c931ef0e31af0e65b3c3da2d4f
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2017-12-13 18:34:00 +0800

    refactor: tidy docs

--- diffusion_python_bot/classes/tqdm_capture.py
+++ diffusion_python_bot/classes/tqdm_capture.py
@@ -3,3 +3,5 @@ from diffusion_python_bot.classes.discord_progress_bar import DiscordProgressBar
 
 class TqdmCapture:
     def __init__(self, progress_bar: DiscordProgressBar, loop, original_stdout, original_stderr):
+        self.progress_bar = progress_bar
+        self.loop = loop
