6cb5bbaf78f68fb6987143771bdfe2f3d5868700
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2017-12-15 09:00:00 +0800

    docs: document progress

--- diffusion_python_bot/classes/tqdm_capture.py
+++ diffusion_python_bot/classes/tqdm_capture.py
@@ -5,3 +5,5 @@ class TqdmCapture:
     def __init__(self, progress_bar: DiscordProgressBar, loop, original_stdout, original_stderr):
         self.progress_bar = progress_bar
         self.loop = loop
+        self.original_stdout = original_stdout
+        self.original_stderr = original_stderr
