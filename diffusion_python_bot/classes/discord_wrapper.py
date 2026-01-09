6590a2fef3a8b6b53356f4c9fb97b88af668b255
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2026-01-09 14:33:00 +0800

    fix: models

--- diffusion_python_bot/classes/discord_wrapper.py
+++ diffusion_python_bot/classes/discord_wrapper.py
@@ -2,3 +2,6 @@ from discord.ext import commands
 class DiscordWrapper(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.forwarded_message_ids = set()
+
+# draft note 1314
