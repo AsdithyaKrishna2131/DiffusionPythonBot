04702aec58223f3115e88d2ccf90c5fd81e87c9b
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2026-06-05 10:30:00 +0800

    fix: models

--- diffusion_python_bot/classes/discord_progress_bar.py
+++ diffusion_python_bot/classes/discord_progress_bar.py
@@ -23,4 +23,4 @@ class DiscordProgressBar:
                 self.progress_message = await self.ctx.send(progress_text)
             else:
                 await self.progress_message.edit(content=progress_text)
-# draft note 1349
+# draft note 1364
